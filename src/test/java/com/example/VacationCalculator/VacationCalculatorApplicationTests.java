60c2fe21ec2d64adfdbb3df267df51bf0c9dc814
bdybgs <bondarev.mc.2018@gmail.com>
2024-09-04 01:07:28 +0300

    test: add unit tests for VacationCalculator service and controller

--- src/test/java/com/example/VacationCalculator/VacationCalculatorApplicationTests.java
+++ src/test/java/com/example/VacationCalculator/VacationCalculatorApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.VacationCalculator;
 
+import com.example.VacationCalculator.controller.VacationCalculatorController;
+import com.example.VacationCalculator.service.VacationCalculatorService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 class VacationCalculatorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void calculationShouldBeTrue() {
+		VacationCalculatorController vacationCalculatorController =
+				new VacationCalculatorController(new VacationCalculatorService());
+		var res = vacationCalculatorController.get(365000, 12);
+		Assertions.assertEquals(12000, res);
+	}
+
+	@Test
+	void calculationShouldBeZero() {
+		VacationCalculatorController vacationCalculatorController =
+				new VacationCalculatorController(new VacationCalculatorService());
+		var res = vacationCalculatorController.get(100000, 0);
+		Assertions.assertEquals(0, res);
 	}
 
 }
